1826e5fd8104c18f2bbb73431cd5295f62a692b8
Maximiliano Hitter <89142014+MaximilianoHitter@users.noreply.github.com>
2022-05-20 14:16:42 -0300

    Delete PEyLW/tpfinalpeylw directory

--- PEyLW/tpfinalpeylw/js/fecha.js
+++
@@ -1,8 +0,0 @@
-//Seleccionar el span
-const spanFecha = document.querySelector('.fecha');
-//Obtener el año
-const fechaActual = new Date().getFullYear() - 15;
-console.log(fechaActual);
-//Agregar el año al span
-spanFecha.textContent = `${fechaActual}`;
-
