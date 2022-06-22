1f9785217c860c2677aaa9748643c43911cb0f45
Maximiliano Hitter <mr.vonhumbolt@gmail.com>
2022-06-22 13:45:05 -0300

    avance

--- IPOO/tpfinal/Entrega/Pasajero.php
+++ IPOO/tpfinal/Entrega/Pasajero.php
@@ -1,13 +1,22 @@
 <?php
+require_once('BaseDatos.php');
 class Pasajero{
     //Atributos
     private $nombre;
     private $apellido;
     private $numDni;
     private $telefono;
+    //Agregar referencia a viaje
 
     //Constructo
-    public function __construct($nombre, $apellido, $numDni, $telefono){
+    public function __construct(){
+        $this->nombre = '';
+        $this->apellido = '';
+        $this->numDni = '';
+        $this->telefono = '';
+    }
+
+    public function cargar($nombre, $apellido, $numDni, $telefono){
         $this->nombre = $nombre;
         $this->apellido = $apellido;
         $this->numDni = $numDni;
@@ -18,31 +27,24 @@ class Pasajero{
     public function getNombre(){
         return $this->nombre;
     }
-
     public function setNombre($nombre){
         $this->nombre = $nombre;
     }
-    
     public function getApellido(){
         return $this->apellido;
     }
-
     public function setApellido($apellido){
         $this->apellido = $apellido;
     }
-
     public function getNumDni(){
         return $this->numDni;
     }
-
     public function setNumDni($numDni){
         $this->numDni = $numDni;
     }
-
     public function getTelefono(){
         return $this->telefono;
     }
-
     public function setTelefono($telefono){
         $this->telefono = $telefono;
     }
@@ -61,4 +63,10 @@ class Pasajero{
         *-----*\n";
         return $str;
     }
+
+    public function Buscar($dni){
+        $base = new BaseDatos();
+        $consultaPasajero = "SELECT * FROM pasajero"
+    }
+
 }
